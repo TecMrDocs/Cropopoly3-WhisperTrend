b354a61015063259765b3b2def46439f9e615573
rn0x7f <ampheriz0@gmail.com>
2025-04-23 22:51:13 -0600

    init: user route

--- core/server/src/api/routes/mod.rs
+++ core/server/src/api/routes/mod.rs
@@ -1,12 +1,15 @@
 mod user;
 
-use actix_web::middleware::from_fn;
+// use actix_web::middleware::from_fn;
 use actix_web::web;
 
 pub fn config_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/user")
-            .service(web::resource("/").route(web::get().to(user::get_users::get_users)))
+            .service(web::resource("/")
+            .route(web::get().to(user::get_users::get_users))
+            .route(web::post().to(user::create_user::create_user))
+        )
     );
     
 }
