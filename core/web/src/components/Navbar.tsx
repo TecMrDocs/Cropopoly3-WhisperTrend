ec6d7cc20414b95f4c44e60328e3e50c17b0949d
SsebasA <sebastian.antonio1707@gmail.com>
2025-05-13 18:03:43 +0000

    Update navbar

--- core/web/src/components/Navbar.tsx
+++ core/web/src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './styles/Navbar.css';
-
+import logo from '../assets/Wtw1.png';
 
 export default function Navbar() {
   return(
     <div className="navbar-styles">
-      <p>Whisper Trend</p>
+      <div className="logo-container">
+        <img src={logo} alt="Whisper Trend Logo" />
+        <p>Whisper Trend</p>
+      </div>
     </div>
   );
 }
