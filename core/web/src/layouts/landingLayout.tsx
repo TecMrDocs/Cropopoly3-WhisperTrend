26b8f75c3a4e8cdf14f5509a1b9db9d86089fc15
SsebasA <sebastian.antonio1707@gmail.com>
2025-05-13 16:23:22 +0000

    Added stylesheets

--- core/web/src/layouts/landingLayout.tsx
+++ core/web/src/layouts/landingLayout.tsx
@@ -1,62 +1,24 @@
 import React from "react";
-
+import './styles/landingLayout.css';
 export default function LandingLayout(props: any){
   const { children } = props;
 
-  
+
 
   return(
-    <div style={{
-      minHeight: '100vh',
-      width: '100vw',
-      margin: 0,
-      padding: 0,
-      position: 'relative',
-      display: 'flex',
-      flexDirection: 'column',
-      overflow: 'hidden'
-    }}>
-      <div style={{
-        minHeight: '85vh',
-        width: '100%',
-        background: 'linear-gradient(180deg, #00BFB3 0%, #0091D5 100%)',
-        position: 'relative',
-        margin: 0,
-        padding: 0
-      }}>
-        <div style={{
-          width: '100%',
-          height: '100%',
-          position: 'relative',
-          zIndex: 1,
-          padding: '20px'
-        }}>
-          <header style={{
-            marginBottom: '20px',
-            paddingBottom: '20px'
-          }}>
-            <h1 style={{ color: 'white' }}>Whisper Trend</h1>
+    <div className="page-container">
+      <div className="page-gradient">
+        <div className="title-container">
+          <header className="header-container">
+            <h1 className="title-style">Whisper Trend</h1>
           </header>
           <main>
             {children}
           </main>
         </div>
       </div>
-      <div style={{
-        minHeight: '25vh',
-        width: '100%',
-        backgroundColor: 'white',
-        position: 'relative',
-        margin: '-10vh 0 0 0',
-        padding: 0
-      }}>
-        <div style={{
-          width: '100%',
-          height: '100%',
-          position: 'relative',
-          zIndex: 1,
-          padding: '20px'
-        }}>
+      <div className="bottom-container">
+        <div className="bottom-content-container">
         </div>
       </div>
     </div>
