04273966d39b0ca8dc4b3ebaf4c4cdf8d450f06c
Arturo BM <a01168331@tec.mx>
2025-05-14 03:49:25 +0000

    Progress bar

---
+++ core/web/src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { LiaIndustrySolid } from "react-icons/lia";
+import { GiCardboardBox } from "react-icons/gi";
+import { LuChartNoAxesCombined } from "react-icons/lu";
+import { MdOutlineTravelExplore } from "react-icons/md";
+
+export default function ProgressBar(){
+  return(
+    <div>
+      <LiaIndustrySolid />
+      <GiCardboardBox />
+      <LuChartNoAxesCombined />
+      <MdOutlineTravelExplore />
+    </div>
+  );
+}
--- core/web/src/pages/LaunchProcess.tsx
+++ core/web/src/pages/LaunchProcess.tsx
@@ -1,7 +1,10 @@
+import ProgressBar from "../components/ProgressBar";
+
 export default function LaunchProcess() {
   return(
     <div>
       <h1>LaunchProcess</h1>
+      <ProgressBar />
     </div>
   );
 }
