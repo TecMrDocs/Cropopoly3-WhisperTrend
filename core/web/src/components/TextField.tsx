8721d5193fe75e5e20c59210a40363b380e6f94b
SsebasA <sebastian.antonio1707@gmail.com>
2025-05-14 17:47:19 +0000

    Updated Profile Page and TextField component

--- core/web/src/components/TextField.tsx
+++ core/web/src/components/TextField.tsx
@@ -1,8 +1,13 @@
-
-export default function TextField(){
+export default function TextField({ label, width = "300px" }: { label?: string, width?: string }){
   return(
-   <div className="p-[3px] rounded-[10px] bg-gradient-to-r from-[#00BFB3] to-[#0091D5] inline-block">
-    <input className="border-none outline-none p-2 px-3 rounded-[6px] bg-white w-[200px] text-base block text-black" />
-   </div>
+    <div className="flex flex-col gap-1">
+      {label && <label className="text-sm text-gray-700">{label}</label>}
+      <div
+        className="p-[3px] rounded-3xl bg-gradient-to-r from-[#00BFB3] to-[#0091D5] inline-block"
+        style={{ width }}
+      >
+        <input className="border-none outline-none p-2 px-3 rounded-3xl bg-white w-full text-base block text-black" />
+      </div>
+    </div>
   );
 }
--- core/web/src/pages/Perfil.tsx
+++ core/web/src/pages/Perfil.tsx
@@ -1,7 +1,26 @@
+import TextField from "../components/TextField";
+
 export default function Perfil() {
   return(
     <div>
-      <h1>Perfil</h1>
+      <div className='flex items-center justify-center'>
+        <h1 className="text-2xl font-w700">Edita tus datos personales</h1>
+      </div>
+      <br></br>
+      <div className='flex flex-row gap-6 justify-center'>
+         <TextField label="Nombre(s)" width="300px" /> 
+         <TextField label="Apellido(s)" width="300px" />
+      </div>
+      <br></br>
+      <div className="flex flex-col gap-5 items-center justify-center">
+        <TextField label="Correo electrónico" width="600px" />
+        <TextField label="Confirma tu correo" width="600px" />
+        <TextField label="Número telefónico" width="600px" />
+        <TextField label="Puesto o cargo en la empresa" width="600px" />
+        <TextField label="Contraseña" width="600px" />
+        <TextField label="Confirma tu contraseña" width="600px" />
+
+      </div>
     </div>
   );
 }
