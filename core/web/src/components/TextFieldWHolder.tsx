58000f36c0c24b3e9fa96de0b49eb444f340d92e
SsebasA <sebastian.antonio1707@gmail.com>
2025-05-16 15:33:37 +0000

    Updated TextFieldWHolder and Profile page

--- core/web/src/components/TextFieldWHolder.tsx
+++ core/web/src/components/TextFieldWHolder.tsx
@@ -3,23 +3,28 @@ type TextFieldWHolderProps = {
   width?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label?: string;
 };
 
 export default function TextFieldWHolder({ 
   placeholder = '', 
   width = '200px',
   value,
-  onChange, 
+  onChange,
+  label,
 }: TextFieldWHolderProps) {
   return (
-    <div className="p-[3px] rounded-[10px] bg-gradient-to-r from-[#00BFB3] to-[#0091D5] inline-block">
-      <input
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        style={{ width }}
-        className="border-none outline-none p-2 px-3 rounded-[6px] bg-white text-base block text-black"
-      />
+    <div className="flex flex-col gap-1">
+      {label && <label className="text-sm text-gray-700">{label}</label>}
+      <div className="p-[3px] rounded-3xl bg-gradient-to-r from-[#00BFB3] to-[#0091D5] inline-block">
+        <input
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          style={{ width }}
+          className="border-none outline-none p-2 px-3 rounded-3xl bg-white text-base block text-black"
+        />
+      </div>
     </div>
   );
 }
--- core/web/src/pages/Perfil.tsx
+++ core/web/src/pages/Perfil.tsx
@@ -1,6 +1,7 @@
 import WhiteButton from "../components/WhiteButton";
 import BlueButton from "../components/BlueButton";
 import TextField from "../components/TextField";
+import TextFieldWHolder from "../components/TextFieldWHolder";
 
 export default function Perfil() {
   return(
@@ -10,17 +11,17 @@ export default function Perfil() {
       </div>
       <br></br>
       <div className='flex flex-row gap-6 justify-center'>
-         <TextField label="Nombre(s)" width="300px" /> 
-         <TextField label="Apellido(s)" width="300px" />
+         <TextFieldWHolder placeholder="Ingrese su nombre" width="300px" label="Nombre(s)"/>
+         <TextFieldWHolder placeholder="Ingrese su apellido(s)" width="300px" label="Apellido(s)" />
       </div>
       <br></br>
       <div className="flex flex-col gap-5 items-center justify-center">
-        <TextField label="Correo electrónico" width="600px" />
-        <TextField label="Confirma tu correo" width="600px" />
-        <TextField label="Número telefónico" width="600px" />
-        <TextField label="Puesto o cargo en la empresa" width="600px" />
-        <TextField label="Contraseña" width="600px" />
-        <TextField label="Confirma tu contraseña" width="600px" />
+        <TextFieldWHolder placeholder="mail@example.com" width="600px" label="Correo electrónico" />
+        <TextFieldWHolder label="Confirma tu correo" width="600px" />
+        <TextFieldWHolder label="Número telefónico" width="600px" placeholder="+55 12 1234 5678" />
+        <TextFieldWHolder label="Puesto o cargo en la empresa" width="600px" />
+        <TextFieldWHolder label="Contraseña" width="600px" />
+        <TextFieldWHolder label="Confirma tu contraseña" width="600px" />
       </div>
       <br></br>
 
