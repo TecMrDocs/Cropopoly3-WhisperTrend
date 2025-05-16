475c0bd87f6ef544377570a0c1f08df56614235a
SsebasA <sebastian.antonio1707@gmail.com>
2025-05-16 15:48:30 +0000

    Updated button sizes

--- core/web/src/components/BlueButton.tsx
+++ core/web/src/components/BlueButton.tsx
@@ -7,7 +7,7 @@ type BlueButtonProps = {
 export default function BlueButton({text = '', width = '400px', onClick} : BlueButtonProps){
   return(
     <div>
-      <button onClick={onClick} style={{ width }} className="text-white bg-gradient-to-r from-blue-600 to-emerald-400 rounded-[15px] border-transparent py-2 px-2.5 text-[17px] font-semibold cursor-pointer w-[8%] hover:from-blue-600 hover:to-emerald-400">
+      <button onClick={onClick} style={{ width }} className="text-white bg-gradient-to-r from-blue-600 to-emerald-400 rounded-3xl border-transparent py-2 px-2.5 text-[17px] font-semibold cursor-pointer w-[8%] hover:from-blue-600 hover:to-emerald-400 h-[48px] ">
         {text}
       </button>
     </div>
