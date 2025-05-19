ddfd48517735e8e66b42bbcd650b314625ea55b2
rn0x7f <ampheriz0@gmail.com>
2025-05-19 02:13:13 -0600

    update: user model

--- core/server/app/src/models/user.rs
+++ core/server/app/src/models/user.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Credentials {
     pub email: String,
-    pub password: String,
+    pub contrasena: String,
 }
 
 #[derive(Validate, Clone)]
@@ -17,11 +17,31 @@ pub struct User {
     #[serde(skip_deserializing)]
     #[diesel(deserialize_as = i32)]
     pub id: Option<i32>,
-    #[validate(length(min = 1, max = 20))]
-    pub username: String,
     #[validate(length(min = 1, max = 255))]
     pub email: String,
+    #[validate(length(min = 1, max = 20))]
+    pub nombres: String,
+    #[validate(length(min = 1, max = 20))]
+    pub apellidos: String,
+    #[validate(length(min = 1, max = 20))]
+    pub telefono: String,
+    #[validate(length(min = 1, max = 20))]
+    pub puesto: String,
     #[validate(length(min = 1, max = 150))]
     #[serde(skip_serializing)]
-    pub password: String,
+    pub contrasena: String,
+    #[validate(length(min = 1, max = 20))]
+    pub plan: String,
+    #[validate(length(min = 1, max = 20))]
+    pub razon_social: String,
+    #[validate(length(min = 1, max = 20))]
+    pub sector: String,
+    #[validate(length(min = 1, max = 20))]
+    pub tamano_empresa: String,
+    #[validate(length(min = 1, max = 20))]
+    pub alcance: String,
+    #[validate(length(min = 1, max = 20))]
+    pub localidades: String,
+    #[validate(length(min = 1, max = 20))]
+    pub num_sucursales: String,
 }
