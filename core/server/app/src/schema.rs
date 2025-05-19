cf80f5936ddbdacb5712d92407c011b8e760841b
rn0x7f <ampheriz0@gmail.com>
2025-05-19 02:12:59 -0600

    add: user db schema

--- core/server/app/src/schema.rs
+++ core/server/app/src/schema.rs
@@ -3,11 +3,31 @@
 diesel::table! {
     users (id) {
         id -> Int4,
-        #[max_length = 20]
-        username -> Varchar,
-        #[max_length = 255]
+        #[max_length = 50]
         email -> Varchar,
+        #[max_length = 50]
+        nombres -> Varchar,
+        #[max_length = 50]
+        apellidos -> Varchar,
+        #[max_length = 50]
+        telefono -> Varchar,
+        #[max_length = 50]
+        puesto -> Varchar,
         #[max_length = 150]
-        password -> Varchar,
+        contrasena -> Varchar,
+        #[max_length = 50]
+        plan -> Varchar,
+        #[max_length = 50]
+        razon_social -> Varchar,
+        #[max_length = 50]
+        sector -> Varchar,
+        #[max_length = 50]
+        tamano_empresa -> Varchar,
+        #[max_length = 50]
+        alcance -> Varchar,
+        #[max_length = 250]
+        localidades -> Varchar,
+        #[max_length = 50]
+        num_sucursales -> Varchar,
     }
 }
