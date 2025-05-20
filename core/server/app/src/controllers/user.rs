f2a072467df8a9e1c5586501087032dc16b7112f
A01749581 <117104241+A01749581@users.noreply.github.com>
2025-05-20 23:25:12 +0000

    Usuario controller

--- core/server/app/src/controllers/user.rs
+++ core/server/app/src/controllers/user.rs
@@ -1,5 +1,4 @@
 use crate::{
-    config::{Claims, Config},
     database::DbResponder,
     middlewares,
     models::{Credentials, User},
@@ -13,49 +12,34 @@ use serde_json::json;
 use tracing::error;
 use validator::Validate;
 
-#[post("/register")]
-pub async fn register(mut user: web::Json<User>) -> Result<impl Responder> {
+#[post("")]
+pub async fn create_user(mut user: web::Json<User>) -> Result<impl Responder> {
     if let Err(_) = user.validate() {
         return Ok(HttpResponse::Unauthorized().body("Invalid data"));
     }
 
-    if let Ok(hash) = PasswordHasher::hash(&user.contrasena) {
-        if let Ok(None) = User::get_by_email(user.email.clone()).await {
-            user.contrasena = hash.to_string();
+    if let Ok(None) = User::get_by_email(user.email.clone()).await {
+        if let Ok(hash) = PasswordHasher::hash(&user.password) {
+            user.password = hash.to_string();
 
             let id = User::create(user.clone()).await.to_web()?;
             user.id = Some(id);
 
-            return Ok(HttpResponse::Ok().json(user));
+            return Ok(HttpResponse::Ok().finish());
         }
-
+    } else {
         return Ok(HttpResponse::Unauthorized().body("Email already exists"));
     }
 
     Err(error::ErrorBadRequest("Failed"))
 }
 
-#[post("/signin")]
-pub async fn signin(profile: web::Json<Credentials>) -> impl Responder {
-    if let Ok(Some(user)) = User::get_by_email(profile.email.clone()).await {
-        if let Ok(true) = PasswordHasher::verify(&profile.contrasena, &user.contrasena) {
-            if let Some(id) = user.id {
-                if let Ok(token) =
-                    TokenService::<Claims>::create(&Config::get_secret_key(), Claims::new(id))
-                {
-                    return HttpResponse::Ok().json(json!({ "token": token }));
-                }
-            }
-        }
-    }
-
-    HttpResponse::Unauthorized().body("Email or password is incorrect")
-}
-
-#[get("")]
-pub async fn check(req: HttpRequest) -> Result<impl Responder> {
-    if let Some(id) = req.extensions().get::<i32>() {
-        let user = User::get_by_id(*id).await.to_web()?;
+// Obtener usuario por id
+#[get("/{id}")]
+pub async fn get_user(req: HttpRequest) -> Result<impl Responder> {
+    if let Some(id) = req.match_info().get("id") {
+        let id = id.parse::<i32>().map_err(|_| error::ErrorBadRequest("Invalid ID"))?;
+        let user = User::get_by_id(id).await.to_web()?;
 
         return match user {
             Some(user) => Ok(HttpResponse::Ok().json(user)),
@@ -67,13 +51,16 @@ pub async fn check(req: HttpRequest) -> Result<impl Responder> {
     Ok(HttpResponse::Unauthorized().finish())
 }
 
+#[get("")]
+pub async fn get_all_users() -> Result<impl Responder> {
+    let users = User::get_all().await.to_web()?;
+
+    return Ok(HttpResponse::Ok().json(users));
+}
+
 pub fn routes() -> actix_web::Scope {
-    web::scope("/auth")
-        .service(register)
-        .service(signin)
-        .service(
-            web::scope("/check")
-                .wrap(from_fn(middlewares::auth))
-                .service(check)
-        )
+    web::scope("/user")
+        .service(create_user)
+        .service(get_user)
+        .service(get_all_users)
 }
