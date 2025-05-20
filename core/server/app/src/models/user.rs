b0c3db5e4f753f859de9d9ad7188ed2521c87d61
rn0x7f <ampheriz0@gmail.com>
2025-05-20 10:54:07 -0600

    setup: user controller, no funciona hasta que no hagan el controller

--- core/server/app/src/models/user.rs
+++ core/server/app/src/models/user.rs
@@ -12,7 +12,7 @@ pub struct Credentials {
 #[derive(Validate, Clone)]
 #[macros::diesel_default(schema::users)]
 #[diesel(primary_key(id))]
-#[macros::database(create, update(id), delete(id), get(email, id))]
+#[macros::database(create, update(id), delete(id), get(email, id), get_all)]
 pub struct User {
     #[serde(skip_deserializing)]
     #[diesel(deserialize_as = i32)]
