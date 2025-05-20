92e446d3cc603864b90fb7804ddfc09a60015eff
Carlos Zamudio <a01799283@tec.mx>
2025-05-20 14:01:07 -0600

    update

---
+++ core/server/app/src/scraping/notices.rs
@@ -0,0 +1,9 @@
+use reqwest::blocking::get;
+
+pub struct NoticesScraper;
+
+impl NoticesScraper {
+    pub fn get_notices() {
+        let url = "https://api.gdeltproject.org/api/v2/doc/doc?query=";
+    }
+}
