234dcc0a1d0af3ee53cc3d5854cf805cbac03ded
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2025-05-22 06:46:11 +0000

    Login a insta con cookies, scraper funciona captio

--- core/server/app/src/controllers/web.rs
+++ core/server/app/src/controllers/web.rs
@@ -2,7 +2,6 @@ use crate::scraping::{
     instagram::InstagramScraper,
     notices::{NoticesScraper, Params},
     reddit::RedditScraper,
-    trends::TrendsScraper,
 };
 use actix_web::{HttpResponse, Responder, get, post, web};
 use serde::{Deserialize, Serialize};
@@ -16,15 +15,15 @@ struct Query {
     language: String,
 }
 
-#[get("/reddit/get-simple-posts-with-members/{keyword}")]
-pub async fn get_simple_posts_with_members_reddit(path: web::Path<String>) -> impl Responder {
-    let posts = RedditScraper::get_simple_posts_with_members(path.into_inner()).await;
-    HttpResponse::Ok().json(posts)
-}
-
 #[get("/reddit/get-simple-posts/{keyword}")]
 pub async fn get_simple_posts_reddit(path: web::Path<String>) -> impl Responder {
     let posts = RedditScraper::get_simple_posts_by_keyword(path.into_inner()).await;
+    HttpResponse::Ok().json(posts)
+}
+
+#[get("/reddit/get-simple-posts-with-members/{keyword}")]
+pub async fn get_simple_posts_with_members_reddit(path: web::Path<String>) -> impl Responder {
+    let posts = RedditScraper::get_simple_posts_with_members(path.into_inner()).await;
     HttpResponse::Ok().json(posts)
 }
 
@@ -36,9 +35,7 @@ pub async fn get_notices(query: web::Json<Query>) -> actix_web::Result<impl Resp
         Ok(date) => date,
         Err(e) => {
             warn!("Failed to parse start date: {}", e);
-            return Err(actix_web::error::ErrorBadRequest(
-                "Invalid start date format",
-            ));
+            return Err(actix_web::error::ErrorBadRequest("Invalid start date format"));
         }
     };
 
@@ -69,9 +66,7 @@ pub async fn get_details(query: web::Json<Query>) -> actix_web::Result<impl Resp
         Ok(date) => date,
         Err(e) => {
             warn!("Failed to parse start date: {}", e);
-            return Err(actix_web::error::ErrorBadRequest(
-                "Invalid start date format",
-            ));
+            return Err(actix_web::error::ErrorBadRequest("Invalid start date format"));
         }
     };
 
@@ -94,45 +89,12 @@ pub async fn get_details(query: web::Json<Query>) -> actix_web::Result<impl Resp
     }
 }
 
-#[post("/trends/get-trends")]
-pub async fn get_trends(query: web::Json<Query>) -> actix_web::Result<impl Responder> {
-    let query = query.into_inner();
-
-    let start_date = match chrono::NaiveDate::parse_from_str(&query.startdatetime, "%Y-%m-%d") {
-        Ok(date) => date,
-        Err(e) => {
-            warn!("Failed to parse start date: {}", e);
-            return Err(actix_web::error::ErrorBadRequest(
-                "Invalid start date format",
-            ));
-        }
-    };
-
-    let end_date = match chrono::NaiveDate::parse_from_str(&query.enddatetime, "%Y-%m-%d") {
-        Ok(date) => date,
-        Err(e) => {
-            warn!("Failed to parse end date: {}", e);
-            return Err(actix_web::error::ErrorBadRequest("Invalid end date format"));
-        }
-    };
-
-    let params = Params::new(query.query, start_date, end_date, query.language);
-
-    match TrendsScraper::get_trends(params).await {
-        Ok(trends) => Ok(HttpResponse::Ok().json(trends)),
-        Err(e) => {
-            warn!("Failed to get trends: {}", e);
-            Err(actix_web::error::ErrorBadRequest("Failed to get trends"))
-        }
-    }
-}
-
 #[get("/instagram/login")]
 pub async fn login_instagram() -> impl Responder {
     match InstagramScraper::login().await {
-        Ok(msg) => HttpResponse::Ok().json(serde_json::json!({
+        Ok(_) => HttpResponse::Ok().json(serde_json::json!({
             "status": "success",
-            "message": msg,
+            "message": "Login exitoso"
         })),
         Err(e) => HttpResponse::InternalServerError().json(serde_json::json!({
             "status": "error",
@@ -141,11 +103,19 @@ pub async fn login_instagram() -> impl Responder {
     }
 }
 
-#[get("/instagram/get-posts/{hashtag}")]
-pub async fn get_posts_instagram(path: web::Path<String>) -> impl Responder {
-    let hashtag = path.into_inner();
-    let posts = InstagramScraper::get_posts_by_hashtag(&hashtag, 5);
-    HttpResponse::Ok().json(posts)
+
+#[get("/instagram/post")]
+pub async fn get_instagram_post() -> impl Responder {
+    match InstagramScraper::get_one_post().await {
+        Ok(post) => HttpResponse::Ok().json(serde_json::json!({
+            "status": "success",
+            "post": post,
+        })),
+        Err(e) => HttpResponse::InternalServerError().json(serde_json::json!({
+            "status": "error",
+            "message": format!("Scraping failed: {}", e),
+        })),
+    }
 }
 
 pub fn routes() -> actix_web::Scope {
@@ -155,6 +125,5 @@ pub fn routes() -> actix_web::Scope {
         .service(get_notices)
         .service(get_details)
         .service(login_instagram)
-        .service(get_posts_instagram)
-        .service(get_trends)
+        .service(get_instagram_post)
 }
