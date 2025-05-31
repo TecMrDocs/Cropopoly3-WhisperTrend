889a38b0d99e3b4389f050ba640210be456d6451
Carlos Zamudio <a01799283@tec.mx>
2025-05-31 17:22:02 -0600

    add: zbrowser

--- core/zbrowser/zbrowser-utils/src/main.rs
+++ core/zbrowser/zbrowser-utils/src/main.rs
@@ -5,17 +5,19 @@ fn main() -> anyhow::Result<()> {
         .parent()
         .ok_or(anyhow::anyhow!("Could not get workspace root"))?;
 
-    let src_go_dir = workspace_root.join("scrap-core/src-go");
+    let src_go_dir = workspace_root.join("zbrowser-core/src-go");
     let libscraper_h = src_go_dir.join("libscraper.h");
     let libscraper_a = src_go_dir.join("libscraper.a");
-    let bindings = workspace_root.join("scrap-core/bindings.rs");
+    let bindings = workspace_root.join("zbrowser-core/bindings.rs");
+    let common_dir = workspace_root.join("common/common.h");
 
     let _ = std::fs::remove_file(bindings);
     let _ = std::fs::remove_file(libscraper_h);
     let _ = std::fs::remove_file(libscraper_a);
+    let _ = std::fs::remove_file(common_dir);
 
     std::process::Command::new("cargo")
-        .current_dir(workspace_root.join("scrap-core"))
+        .current_dir(workspace_root.join("zbrowser-core"))
         .args(&["clean"])
         .output()?;
 
