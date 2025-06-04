311c7654b55b2fab2d9d8ef2b355bbc8b581c96f
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2025-06-04 03:05:19 +0000

    rebase

--- core/web/src/provider/AuthProvider.tsx
+++ core/web/src/provider/AuthProvider.tsx
@@ -1,4 +1,58 @@
-import { useState, useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import { AuthContext } from '@/contexts/AuthContext';
-import { NavigateFunction } from 'react-router-dom';
+import api from '@/utils/api/admin';
 
+
+export default function AuthProvider({ children }: {children: React.ReactNode}) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if(token) {
+      api.admin
+        .verifyAdmin()
+        .then(() => {
+          setIsAuthenticated(true);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setIsAuthenticated(false);
+          setIsLoading(false);
+        });
+    } else {
+      setIsAuthenticated(false);
+      setIsLoading(false);
+    }
+  }, []);
+
+
+  function signOut() {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+  }
+
+  function signIn(email: string, password: string) {
+    api.admin.loginAdmin(email, password)
+      .then(({ token }) => {
+        localStorage.setItem("token", token);
+        setIsAuthenticated(true);
+      })
+  }
+
+
+  return (
+   <AuthContext.Provider value={{
+      isAuthenticated, 
+      isLoading, 
+      signOut,
+      signIn
+      }
+    }>
+    {children}
+  </AuthContext.Provider>
+  );
+
+  
+}
