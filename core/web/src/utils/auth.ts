222871b200ac6f3f7155aaff50e1f28adbe3b766
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2025-06-04 03:10:41 +0000

    rebase

--- core/web/src/utils/auth.ts
+++ core/web/src/utils/auth.ts
@@ -1,27 +1,15 @@
 interface Config {
   withCredentials: boolean;
   headers: {
-<<<<<<< HEAD
     Authorization: string;
-=======
-    token: string;
->>>>>>> main
   };
 }
 
 export function getConfig(): Config {
-<<<<<<< HEAD
   return {
     withCredentials: true,
     headers: {
       Authorization: 'Bearer ' + localStorage.getItem('token'),
-=======
-  const token = localStorage.getItem('token');
-  return {
-    withCredentials: true,
-    headers: {
-      token: token || "",
->>>>>>> main
     }
   }
 }
