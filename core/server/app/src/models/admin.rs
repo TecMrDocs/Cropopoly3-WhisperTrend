cd8fdd430291b7da465b2e7c4beb2100d8531f9b
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2025-06-04 03:12:27 +0000

    rebase

--- core/server/app/src/models/admin.rs
+++ core/server/app/src/models/admin.rs
@@ -4,39 +4,26 @@ use validator::Validate;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct AdminCredentials {
+pub struct Credentials {
     pub email: String,
-<<<<<<< HEAD
     pub contrasena: String,
-=======
-    pub password: String,
->>>>>>> main
 }
 
 #[derive(Validate, Clone)]
-#[macros::diesel_default(schema::admins)]
+#[macros::diesel_default(schema::users)]
 #[diesel(primary_key(id))]
 #[macros::database(create, update(id), delete(id), get(email, id))]
-pub struct Admin {
+pub struct User {
     #[serde(skip_deserializing)]
     #[diesel(deserialize_as = i32)]
     pub id: Option<i32>,
     #[validate(length(min = 1, max = 255))]
     pub email: String,
     #[validate(length(min = 1, max = 20))]
-<<<<<<< HEAD
     pub nombres: String,
     #[validate(length(min = 1, max = 20))]
     pub apellidos: String,
     #[validate(length(min = 1, max = 150))]
     #[serde(skip_serializing)]
     pub contrasena: String, 
-=======
-    pub name: String,
-    #[validate(length(min = 1, max = 20))]
-    pub last_name: String,
-    #[validate(length(min = 1, max = 150))]
-    #[serde(skip_serializing)]
-    pub password: String, 
->>>>>>> main
 }
