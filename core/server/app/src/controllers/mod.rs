5f855502eeaaf9cd807638f436337b42d106cef1
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2025-06-04 03:02:59 +0000

    rebase

--- core/server/app/src/controllers/mod.rs
+++ core/server/app/src/controllers/mod.rs
@@ -3,7 +3,11 @@ pub mod web;
 pub mod chat;
 pub mod recurso;
 pub mod user;
+<<<<<<< HEAD
 pub mod sale;
 pub mod admin;
 pub mod flow;
 pub mod flow_config;
+=======
+pub mod sale;
+>>>>>>> 2e4f098 (init: sale route, no funciona hasta que no lo creen)
--- core/server/app/src/main.rs
+++ core/server/app/src/main.rs
@@ -39,8 +39,6 @@ impl Application for AppServer {
                         .service(controllers::recurso::routes())
                         .service(controllers::user::routes())
                         .service(controllers::sale::routes())
-                        .service(controllers::admin::routes())
-                        .service(controllers::flow::routes())
                 )
         });
 
