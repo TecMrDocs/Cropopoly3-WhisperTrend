65ac677f98f86be6b34cffc9fe8db1dd7a383017
A01749581 <117104241+A01749581@users.noreply.github.com>
2025-06-09 01:05:34 +0000

    send email

---
+++ core/server/app/src/controllers/email.rs
@@ -0,0 +1,37 @@
+use resend_rs::types::CreateEmailBaseOptions;
+use resend_rs::Resend;
+use actix_web::{post, web, HttpResponse, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct EmailRequest {
+    to: Vec<String>,
+    subject: String,
+    html: String,
+}
+
+#[post("/send")]
+pub async fn send(email_request: web::Json<EmailRequest>) -> impl Responder {
+    let resend = Resend::default(); 
+
+    let from = "Acme <onboarding@resend.dev>";
+    let to: Vec<&str> = email_request.to.iter().map(|s| s.as_str()).collect();
+    let subject = &email_request.subject;
+    let html = &email_request.html;
+
+    let email = CreateEmailBaseOptions::new(from, to, subject).with_html(html);
+
+    match resend.emails.send(email).await {
+        Ok(_) => HttpResponse::Ok().body("Correo enviado con éxito"),
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            HttpResponse::InternalServerError().body("Error al enviar el correo")
+        }
+    }
+}
+
+
+pub fn routes() -> actix_web::Scope {
+    web::scope("/email")
+        .service(send)
+}
--- core/server/app/src/main.rs
+++ core/server/app/src/main.rs
@@ -56,6 +56,7 @@ impl Application for AppServer {
                         .service(controllers::sale::routes())      // Sales-related routes
                         .service(controllers::admin::routes())     // Admin panel routes
                         .service(controllers::flow::routes())      // Flow management routes
+                        .service(controllers::email::routes())     // Email sending routes
                         //.service(nosql::routes())
                 )
                 // Configure static file serving
